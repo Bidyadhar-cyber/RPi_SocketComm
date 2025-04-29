803b752568c016aa0415fc6a3773fc630264cb83
Bidyadhar-cyber <bidyakhd@gmail.com>
2025-04-29 15:37:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
-
+print(r"""
+     __  __  __   ____  ____   __  __ 
+    / / / / / /  / ___|| ___| |  \/  |
+   / / / / / /  | |    |___ \ | |\/| |
+  / / / / / /   | |___  ___) || |  | |
+ /_/ /_/ /_/     \____||____/ |_|  |_|
+              
+""")
 setup(
     name="RPi_SocketComm",
     version="0.1.0",
